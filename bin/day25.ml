9d332d2d504861c3965817b11a68a19854af6187
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-10-05 08:42:00 -0400

    THIS IS AN AUTO-GENERATED COMMIT BECAUSE I WAS TOO LAZY TO MAKE ONE

--- bin/day25.ml
+++ bin/day25.ml
@@ -7,13 +7,8 @@ type spec = {
   heights: int array;
 }
 
-let spec_of_seq seq =
-  None
-
 let parse input =
   input
     |> String.trim
     |> String.split_on_char '\n'
     |> List.to_seq
-
-let input = Input.get_input 25
