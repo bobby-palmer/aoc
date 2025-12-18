0615c19c1497799e9e6d6f7dbb07a65d45fda08c
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-12-18 18:32:35 -0500

    structure

--- bin/day10.ml
+++ bin/day10.ml
@@ -1,23 +1,19 @@
-type item = Goal of int list | Button of int list | Volts of int list
-
-(** Remove first and last charater of string *)
-let str_trim_ends str =
+(** Remove one char from the begining and end of the string *)
+let string_trim_ends str =
   String.sub str 1 (String.length str - 2)
 
-(** Parse one item from a line *)
-let _item_of_str str =
-  match str.[0] with
-  | '[' -> 
-      str
-      |> str_trim_ends
-      |> String.to_seqi
-      |> Seq.filter (fun (_, letter) -> letter = '#')
-      |> Seq.map (fst)
-      |> List.of_seq
-      |> fun v -> Goal v
+let parse_line line =
+  let sections = line
+    |> String.split_on_char ' '
+    |> List.map string_trim_ends
+  in
+  let goal = List.hd sections
+  and rest = List.tl sections
+  in
+  ()
 
-  | '(' -> Button []
-
-  | '{' -> Volts []
-
-  | _ -> failwith "Bad item str"
+let _parse input =
+  input
+  |> String.trim
+  |> String.split_on_char '\n'
+  |> List.map parse_line
