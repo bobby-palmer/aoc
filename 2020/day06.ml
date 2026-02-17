06d6951f710ffa061f755176f24d43b317065728
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2026-02-17 05:06:10 -0500

    day6

--- 2020/day06.ml
+++ 2020/day06.ml
@@ -4,18 +4,34 @@ let input =
   Input.get ~year:2020 ~day:6
   |> String.split_paragraphs
 
+module CharSet = Set.Make(Char)
+
 let count_yesses answers =
   answers
   |> String.to_seq
   |> Seq.filter ((!=) '\n')
-  |> Seq.map (fun e -> (e, ()))
-  |> Hashtbl.of_seq
-  |> Hashtbl.length
+  |> CharSet.of_seq
+  |> CharSet.cardinal
 
 let part1 input =
   input
   |> List.map count_yesses
   |> List.fold_left (+) 0
 
-(** TODO submit *)
 let () = part1 input |> Printf.printf "Part1: %d\n"
+
+let count_unanimous group =
+  let selections =
+    group
+    |> String.split_on_char '\n'
+    |> List.map (fun line -> line |> String.to_seq |> CharSet.of_seq)
+  in
+  selections
+    |> List.fold_left CharSet.inter (List.hd selections)
+    |> CharSet.cardinal
+
+let () =
+  input
+    |> List.map count_unanimous
+    |> List.fold_left (+) 0
+    |> Printf.printf "Part2: %d\n"
--- lib/string.ml
+++ lib/string.ml
@@ -1,11 +1,3 @@
 include Stdlib.String
 
-let split_paragraphs s =
-  let lines = Stdlib.String.split_on_char '\n' s in
-  let rec aux acc = function
-    | x :: xs when x = "" -> acc :: (aux "" xs)
-    | x :: xs -> aux (acc^"\n"^x) xs
-    | [] -> []
-  in
-  aux "" lines
-
+let split_paragraphs = Str.split (Str.regexp "\n\n+")
