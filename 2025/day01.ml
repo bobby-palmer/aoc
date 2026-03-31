bf581c68dd880fdf1a07e084ded826a9bcafc211
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2026-03-31 11:54:51 -0400

    day1 part2

--- 2025/day01.ml
+++ 2025/day01.ml
@@ -1,29 +1,25 @@
 open Aoc
 
-type direction = Left | Right
-
 let parse line =
   Scanf.sscanf line "%c%d" (fun direction amount -> 
     match direction with
-    | 'L' -> (Left, amount)
-    | 'R' -> (Right, amount)
+    | 'L' -> (-1) * amount
+    | 'R' -> amount
     | _ -> failwith "bad input"
   )
+
+(** Positive mod *)
+let ( %! ) a b =
+  let abs_ans = a mod b in
+  if abs_ans < 0 then 
+    abs_ans + b
+  else 
+    abs_ans
 
 let directions =
   Input.get ~year:2025 ~day:1 |>
   String.split_on_char '\n' |>
   List.map parse
-
-let apply pos (dir, amount) =
-  let abs_pos =
-    match dir with
-    | Left -> pos - amount
-    | Right -> pos + amount
-  in
-  let mod_pos = abs_pos mod 100 in
-  if mod_pos < 0 then mod_pos + 100
-  else mod_pos
 
 let rec count_zero pos = function
   | x :: xs ->
@@ -31,7 +27,7 @@ let rec count_zero pos = function
         if pos = 0 then 1
         else 0
       in
-      let next_pos = apply pos x in
+      let next_pos = (pos + x) %! 100 in
       is_zero + count_zero next_pos xs
   | [] -> 0
 
@@ -39,7 +35,31 @@ let () =
   let zeros = count_zero 50 directions in
   Printf.printf "Part1: %d\n" zeros
 
+let count_intermediate_zeros pos move =
+  let normalized_right =
+    if move > 0 then
+      pos + move
+    else
+      ((pos + move) %! 100) - move
+  in
+  if normalized_right mod 100 = 0 then
+    normalized_right / 100 - 1
+  else 
+    normalized_right / 100
 
-(* let () = *)
-(*   let zeroes = count_zero 50 directions in *)
-(*   Printf.printf "Part2: %d\n" zeroes *)
+let rec count_zero pos = function
+  | x :: xs ->
+      let next_pos = (pos + x) %! 100 in
+      let zeros_for_move = 
+        if next_pos = 0 then
+          1 + count_intermediate_zeros pos x
+        else
+          count_intermediate_zeros pos x 
+      in
+      zeros_for_move + count_zero next_pos xs
+  | [] -> 0
+
+
+let () =
+  let zeroes = count_zero 50 directions in
+  Printf.printf "Part2: %d\n" zeroes
